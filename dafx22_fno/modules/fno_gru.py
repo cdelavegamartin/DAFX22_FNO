920d2714bf4e3e5827ff095f0fb0bd6f9aa4f2a5
cdelavegamartin <c.delavega.martin@protonmail.com>
2023-02-28 18:51:56 +0000

    Change deprecated calls and add formatting

--- dafx22_fno/modules/fno_gru.py
+++ dafx22_fno/modules/fno_gru.py
@@ -1,6 +1,7 @@
 import torch
 from .layers import FourierConv1d, FourierConv2d
 
+
 class FNO_GRU_1d(torch.nn.Module):
     def __init__(self, in_channels, out_channels, spatial_size, width):
         super(FNO_GRU_1d, self).__init__()
@@ -19,21 +20,27 @@ class FNO_GRU_1d(torch.nn.Module):
 
     def forward(self, x, num_time_steps):
         x = self.in_mapping(x)
-        output = torch.zeros(x.shape[0], num_time_steps,x.shape[1],self.out_channels).to(x.device)
+        output = torch.zeros(
+            x.shape[0], num_time_steps, x.shape[1], self.out_channels
+        ).to(x.device)
         for i in range(num_time_steps):
-          x = self.cell(x)
-          output[:,i,:,:] = self.out_mapping(x)
+            x = self.cell(x)
+            output[:, i, :, :] = self.out_mapping(x)
         return output
+
     def cell(self, h):
         h_in = h.permute(0, 2, 1)
-        z = torch.nn.functional.sigmoid(self.z_layer(h_in))
-        r = torch.nn.functional.sigmoid(self.r_layer(h_in))
-        new_h = torch.nn.functional.tanh(self.h_layer(r * h_in))
+        z = torch.sigmoid(self.z_layer(h_in))
+        r = torch.sigmoid(self.r_layer(h_in))
+        new_h = torch.tanh(self.h_layer(r * h_in))
         h = (1 - z) * h_in + z * new_h
-        return h.permute(0,2,1)
+        return h.permute(0, 2, 1)
+
 
 class FNO_GRU_2d(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, spatial_size_x, spatial_size_y, width):
+    def __init__(
+        self, in_channels, out_channels, spatial_size_x, spatial_size_y, width
+    ):
         super(FNO_GRU_2d, self).__init__()
 
         self.in_channels = in_channels
@@ -42,23 +49,32 @@ class FNO_GRU_2d(torch.nn.Module):
 
         self.in_mapping = torch.nn.Linear(in_channels, self.width)
 
-        self.z_layer = FourierConv2d(self.width, self.width, spatial_size_x, spatial_size_y)
-        self.r_layer = FourierConv2d(self.width, self.width, spatial_size_x, spatial_size_y)
-        self.h_layer = FourierConv2d(self.width, self.width, spatial_size_x, spatial_size_y)
+        self.z_layer = FourierConv2d(
+            self.width, self.width, spatial_size_x, spatial_size_y
+        )
+        self.r_layer = FourierConv2d(
+            self.width, self.width, spatial_size_x, spatial_size_y
+        )
+        self.h_layer = FourierConv2d(
+            self.width, self.width, spatial_size_x, spatial_size_y
+        )
 
         self.out_mapping = torch.nn.Linear(self.width, out_channels)
 
     def forward(self, x, num_time_steps):
         x = self.in_mapping(x)
-        output = torch.zeros(x.shape[0], num_time_steps,x.shape[1], x.shape[2], self.out_channels).to(x.device)
+        output = torch.zeros(
+            x.shape[0], num_time_steps, x.shape[1], x.shape[2], self.out_channels
+        ).to(x.device)
         for i in range(num_time_steps):
-          x = self.cell(x)
-          output[:,i,:,:,:] = self.out_mapping(x)
+            x = self.cell(x)
+            output[:, i, :, :, :] = self.out_mapping(x)
         return output
+
     def cell(self, h):
         h_in = h.permute(0, 3, 1, 2)
-        z = torch.nn.functional.sigmoid(self.z_layer(h_in))
-        r = torch.nn.functional.sigmoid(self.r_layer(h_in))
-        new_h = torch.nn.functional.tanh(self.h_layer(r * h_in))
+        z = torch.sigmoid(self.z_layer(h_in))
+        r = torch.sigmoid(self.r_layer(h_in))
+        new_h = torch.tanh(self.h_layer(r * h_in))
         h = (1 - z) * h_in + z * new_h
-        return h.permute(0,2,3,1)
+        return h.permute(0, 2, 3, 1)
